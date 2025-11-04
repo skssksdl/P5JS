6e107d67d7bf7feeb5c53f67455e34ba4c88f81a
skssksdl <jch0212216@gmail.com>
2025-11-04 12:11:46 +0900

    ㅣ

--- sketch.js
+++ sketch.js
@@ -10,9 +10,21 @@ function setup() {
 function draw() {
   background(220);
 
-  let gravity = createVector(0, 0.3);
+  let gravity = createVector(0, 0.01);
   ball.addForce(gravity);
-
   ball.update();
   ball.show();
+}
+
+function mouseReleased() {
+
+let d = dist(mouseX, mouseY, ball.pos.x, ball.pos.y);
+
+if (d < ball.w/2) {
+
+  let ff = p5.Vector.sub(ball.pos, createVector(mouseX, mouseY));
+  ff.mult(0.3);
+  ball.addForce(ff);
+
+}
 }
